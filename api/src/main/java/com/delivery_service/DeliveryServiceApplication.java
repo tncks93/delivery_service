0cd384be4dfc9ca09e71c8b84db4deaf57c18fc9
tncks <tncks93@gmail.com>
2024-12-19 16:51:32 +0900

    [#3] Feat: Owner용 Shop DTO,Repository 인터페이스 및 Entity 구현

--- api/src/main/java/com/delivery_service/DeliveryServiceApplication.java
+++ api/src/main/java/com/delivery_service/DeliveryServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class DeliveryServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(DeliveryServiceApplication.class, args);
     }
 }
-
-
