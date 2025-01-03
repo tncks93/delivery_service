1c0540c042a024940554de65459f35e2e12a2db2
tncks <tncks93@gmail.com>
2025-01-03 16:07:55 +0900

    [#6] Feat: Owner용 Shop Service 구현
    - 가게등록,조회,수정,open/close 상태 변경 서비스 구현
    - 서비스 구현에 따라 OwnerShopRepository 코드 수정

---
+++ api/src/main/java/com/delivery_service/owners/entity/Owner.java
@@ -0,0 +1,15 @@
+package com.delivery_service.owners.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Owner {
+    private Integer id;
+    private Integer shopId;
+
+
+}
---
+++ api/src/main/java/com/delivery_service/owners/repository/OwnerRepository.java
@@ -0,0 +1,16 @@
+package com.delivery_service.owners.repository;
+
+import com.delivery_service.owners.entity.Owner;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OwnerRepository {
+
+    public Owner findById(int id) {
+        return null;
+    }
+
+    public void saveShopId(Owner owner, Integer shopId) {
+
+    }
+}
