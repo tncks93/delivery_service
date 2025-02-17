4c09d9255b71f9b7908f14cff111bfa9953c1387
tncks <tncks93@gmail.com>
2025-02-17 17:13:28 +0900

    [#38] Feat: 비동기 로직 적용 - ExecutorService 빈 등록 - OwnerShopController 비동기 로직 적용 - ImageUrlController 비동기 로직 적용

--- api/src/main/java/com/delivery_service/common/controller/ImageUrlController.java
+++ api/src/main/java/com/delivery_service/common/controller/ImageUrlController.java
@@ -4,7 +4,10 @@ import com.delivery_service.common.dto.ImageUploadReqDto;
 import com.delivery_service.common.dto.ImageUrlDto;
 import com.delivery_service.common.response.CommonResponse;
 import com.delivery_service.common.service.ImageUrlService;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class ImageUrlController {
 
   private final ImageUrlService imageUrlService;
+  private final ExecutorService executorService;
 
   @PostMapping("/request/image-url")
-  public ResponseEntity<CommonResponse<ImageUrlDto>> getShopImageUrl(
+  public CompletableFuture<ResponseEntity<CommonResponse<ImageUrlDto>>> getShopImageUrl(
       @RequestBody ImageUploadReqDto imageUploadReqDto) {
-    ImageUrlDto imageUrlDto = imageUrlService.getImageUrlDto(
-        imageUploadReqDto.getOriginalImageName());
 
-    CommonResponse<ImageUrlDto> response = CommonResponse.success(imageUrlDto);
-
-    return new ResponseEntity<>(response, HttpStatus.OK);
+    return CompletableFuture.supplyAsync(
+            () -> imageUrlService.getImageUrlDto(imageUploadReqDto.getOriginalImageName()),
+            executorService)
+        .thenApply(CommonResponse::success)
+        .thenApply(res -> new ResponseEntity<>(res, HttpStatus.OK));
   }
+//  @PostMapping("/request/image-url")
+//  public ResponseEntity<CommonResponse<ImageUrlDto>> getShopImageUrl(
+//      @RequestBody ImageUploadReqDto imageUploadReqDto) {
+//
+//    ImageUrlDto imageUrlDto = imageUrlService.getImageUrlDto(
+//        imageUploadReqDto.getOriginalImageName());
+//
+//    CommonResponse<ImageUrlDto> response = CommonResponse.success(imageUrlDto);
+//
+//    return new ResponseEntity<>(response, HttpStatus.OK);
+//  }
 }
