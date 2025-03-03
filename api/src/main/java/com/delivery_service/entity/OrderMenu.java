3572d3238491462499b3688e3bf05ed182a47c44
tncks <tncks93@gmail.com>
2025-03-03 19:56:27 +0900

    [#41] Feat: Order 요청 기능 추가

---
+++ api/src/main/java/com/delivery_service/entity/OrderMenu.java
@@ -0,0 +1,26 @@
+package com.delivery_service.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "order_menu")
+@ToString
+@Setter
+public class OrderMenu {
+
+  //데이터 중복...
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  @Column(name = "order_id")
+  private Integer orderId;
+  private String name;
+  private Integer price;
+}
